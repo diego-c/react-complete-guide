402c74616f6ed264b64a4e629eadf89bf9401d48
diego-c <diechaves@gmail.com>
2017-12-20 20:47:05 -0300

    set up radium with media queries

--- section 5/src/Person/Person.js
+++ section 5/src/Person/Person.js
@@ -4,8 +4,25 @@ import './Person.css';
 
 const Person = props => {
     const styles = {
-        '@media only screen and (min-width: 500px)': {
-            width: '450px'
+        '@media only screen and (max-width: 414px)': {
+            width: '100%',
+            fontSize: '.8rem',
+            padding: 0
+        },
+
+        '@media only screen and (min-width: 414px) and (max-width: 768px)': {
+            width: '80%',
+            fontSize: '1rem'
+        },
+
+        '@media only screen and (min-width: 768px) and (max-width: 1024px)': {
+            width: '70%',
+            fontSize: '1.2rem'
+        },
+
+        '@media only screen and (min-width: 1024px)': {
+            width: '60%',
+            fontSize: '1.4rem'
         }
     };
 
@@ -13,7 +30,7 @@ const Person = props => {
     <div className="Person" style= { styles }>
         <p className="Person__btn" onClick={props.click}>X</p>
         <div className="Person__content">
-            <h1>Hi! My name is { props.name }, I'm {props.age} and my hobbies are: {props.hobbies.join(', ')}.</h1>
+            <h1 style={{textAlign: 'justify'}}>Hi! My name is { props.name }, I'm {props.age} and my hobbies are: {props.hobbies.join(', ')}.</h1>
             
             <input type="text" onChange={props.nameChangedHandler} defaultValue={ props.name } />
         </div>
