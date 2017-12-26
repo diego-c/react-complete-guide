f5beae54e8608fd051ce35dcca8f6af0ec57e793
diego-c <diechaves@gmail.com>
2017-12-26 19:10:51 -0300

    remove other sections

--- lists-conditionals-assignment-problem/src/Validation/Validation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Validation = props => (
-    <div>
-        <p>{props.length >= 5 ? 'Text long enough' : props.length === 0 ? 'Please type something' : 'Text too short!'}</p>
-    </div>
-);
-
-export default Validation;
