e93959c3c9311b481e73a774ebf918dad0291bde
diego-c <diechaves@gmail.com>
2017-12-31 13:27:10 -0300

    section 8 lecture 113

--- my-burger-app-basic/src/components/Burger/Burger.js
+++ my-burger-app-basic/src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = props => {
+    const transformedIngredients = [];
+    Object.keys(props.ingredients).forEach(ingredient => {
+        for (let i = 0; i < props.ingredients[ingredient]; i++) {
+            transformedIngredients.push(<BurgerIngredient type = { ingredient } key = { ingredient + i } />);
+        }
+    });
     return (
         <div className = { classes.Burger }>
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type = "bread-top" />
+            { transformedIngredients }
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- my-burger-app-basic/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-app-basic/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 export default class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            'meat': 2,
+            'cheese': 2,
+            'salad': 1,
+            'bacon': 1
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients = { this.state.ingredients } />
                 <div>Burger controls</div>
             </Aux>
         )
