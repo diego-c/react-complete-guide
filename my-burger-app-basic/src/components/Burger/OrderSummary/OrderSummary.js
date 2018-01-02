0d4a90737b234d5b0a6aac65a8b70eaf175c4faa
diego-c <diechaves@gmail.com>
2018-01-02 20:53:30 -0300

    section 8 lecture 124

--- my-burger-app-basic/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-burger-app-basic/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
     const ingredients = 
@@ -22,6 +23,14 @@ const orderSummary = props => {
                 { ingredients }
             </ul>
             <p>Continue to checkout?</p>
+            <Button
+            btnType = "Danger"
+            >
+                CANCEL
+            </Button>
+            <Button btnType = "Success">
+                CONTINUE
+            </Button>
         </Aux>
     );
 };
