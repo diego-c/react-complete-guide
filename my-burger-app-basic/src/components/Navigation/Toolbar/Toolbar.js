af412ea0a4b1c358502fb1e96a93f009b514befe
diego-c <diechaves@gmail.com>
2018-01-03 13:16:33 -0300

    section 8 lecture 128

--- my-burger-app-basic/src/components/Navigation/Toolbar/Toolbar.js
+++ my-burger-app-basic/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
 
 const toolbar = props => (
     <header 
     className = { classes.Toolbar }>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
