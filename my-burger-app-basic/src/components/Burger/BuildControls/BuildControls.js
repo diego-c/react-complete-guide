4bcea1d47439144ad31ac79c0866822b94640f1a
diego-c <diechaves@gmail.com>
2018-01-03 23:04:04 -0300

    add small style tweaks

--- my-burger-app-basic/src/components/Burger/BuildControls/BuildControls.js
+++ my-burger-app-basic/src/components/Burger/BuildControls/BuildControls.js
@@ -11,7 +11,7 @@ const controls = [
 
 const buildControls = props => (
     <div className = { classes.BuildControls }>
-        <p>Current Price: <strong>{ props.price.toFixed(2) }</strong></p>
+        <p className = { classes.CurrentPrice }>Current Price: <strong>{ props.price.toFixed(2) }</strong></p>
         { controls.map(({ label, type }) => (                           
             <BuildControl 
             key = { label }
--- my-burger-app-basic/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-burger-app-basic/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
+import classes from './OrderSummary.css';
 
 const orderSummary = props => {
     const ingredients = 
@@ -8,21 +9,22 @@ const orderSummary = props => {
     .keys(props.ingredients)
     .filter(ing => props.ingredients[ing])
     .map(ing => (
-        <li        
+        <li
+        className = { classes.OrderItem }        
         key={ ing }>
-        <span style = {{ textTransform: 'capitalize' }}>
+        <span style = {{ textTransform: 'capitalize', color: '#952ab2' }}>
             { ing }
-        </span>: { props.ingredients[ing] }</li>
+        </span>: <strong>{ props.ingredients[ing] }</strong></li>
     ));
 
     return (
         <Aux>
-            <h3>Your order:</h3>
-            <p>A delicious order with the following ingredients:</p>
-            <ul>
+            <h3 className = {classes.OrderTitle }>Your order:</h3>
+            <p className = { classes.OrderSubtitle }>A delicious order with the following ingredients:</p>
+            <ul className = { classes.OrderList }>
                 { ingredients }
             </ul>
-            <p><strong>Total Price: { props.price.toFixed(2) } </strong></p>
+            <p className = { classes.OrderPrice }><strong>Total Price: { props.price.toFixed(2) } </strong></p>
             <p>Continue to checkout?</p>
             <Button
             btnType = "Danger"
