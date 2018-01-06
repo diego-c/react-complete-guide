1fa5fd600a1fb7ec5aae800271d52b1dccd9dfc9
diego-c <diechaves@gmail.com>
2018-01-06 00:27:42 -0300

    section 9 lecture 150

--- section-9/blog-project/src/components/FullPost/FullPost.js
+++ section-9/blog-project/src/components/FullPost/FullPost.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        currentPost: {}
+    }
+
+    async componentWillReceiveProps(nextProps) {
+        if (this.props.id !== nextProps.id) {
+            const post = await axios.get(`https://jsonplaceholder.typicode.com/posts/${nextProps.id}`);
+
+            this.setState({ currentPost: post.data });
+        }
+    }
+
+    /*
+     Alternative:
+     HOWEVER, there's a bug!
+     although we don't get an infinite loop here, the first post clicked doesn't get rendered for some reason
+    */
+
+    /* shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.id !== this.props.id;
+    }
+
+    async componentDidUpdate() {
+        if (this.props.id) {
+            const post = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`);
+
+            this.setState({ currentPost: post.data });
+        }
+    } */
+
     render () {
         let post = null;
         if (!this.props.id) {
@@ -10,14 +41,13 @@ class FullPost extends Component {
         } else {
             post = (
                 <div className="FullPost">
-                    <h1>{ this.props.title }</h1>
-                    <p>{ this.props.content }</p>
+                    <h1>{ this.state.currentPost.title }</h1>
+                    <p>{ this.state.currentPost.body }</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
-                </div>
-
-            );
+                </div>    
+            );          
         }
 
         return post;
