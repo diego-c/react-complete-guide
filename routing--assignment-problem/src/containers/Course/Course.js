336fb6ed12d8761b49547f7246a663ebcbda2f84
diego-c <diechaves@gmail.com>
2018-01-17 19:36:28 -0300

    add better query params error handling and code clean up

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class Course extends Component {    
-    render () {
-        const title = decodeURIComponent(this.props.location.search).split('?title=')[1] || 'Sorry, course not found.';
+    render () {      
+        const { courses } = this.props.location.state || { courses: null };
         const { id } = this.props.match.params;
+
+        let title = 
+        (
+            courses
+            && 
+            courses.find(course => course.id === Number(id))
+        )
+        ? 
+        (courses.find(course => course.id === Number(id))).title
+        : 'Sorry, course not found.';
+
+        if (!courses) {
+            title = `Sorry, the query param doesn't match the course id.`;
+        }
+
         return (
             <div>
                 <h1>{ title }</h1>
