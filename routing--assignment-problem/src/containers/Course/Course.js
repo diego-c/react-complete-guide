3ab54a4a717053b3c721c114a5bf205bdd5f8d64
diego-c <diechaves@gmail.com>
2018-01-17 12:16:20 -0300

    add assignment

---
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    render () {
+        return (
+            <div>
+                <h1>_COURSE_TITLE_</h1>
+                <p>You selected the Course with ID: _ID_</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
---
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import './Courses.css';
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        this.state.courses.map( course => {
+                            return <article className="Course" key={course.id}>{course.title}</article>;
+                        } )
+                    }
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Courses;
