1a9a21cb7127679eccedb738dd6aed4e6b666b6d
diego-c <diechaves@gmail.com>
2018-01-18 20:17:11 -0300

    sync files with master

---
+++ section-12/src/containers/Checkout/Checkout.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+export default class Checkout extends Component {
+    state = {
+        ingredients: {
+            meat: 1,
+            cheese: 1,
+            bacon: 1,
+            salad: 1
+        }
+    }
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredients = { this.state.ingredients } />
+            </div>
+        )
+    }
+}
