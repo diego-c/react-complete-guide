33199cfb42fdbb3515c1fa00749accdbe9b792c3
diego-c <diechaves@gmail.com>
2018-01-19 18:02:36 -0300

    add routes - section 12 lecture 204

--- section-12/src/App.js
+++ section-12/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
+        <Route path="/" exact component = { BurgerBuilder } />
+        <Route path="/checkout" component = { Checkout } />
       </Layout>
     );
   }
--- section-12/src/index.js
+++ section-12/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <Router>
+        <App />
+    </Router>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
