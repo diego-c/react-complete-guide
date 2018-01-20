115796493a27e2f638f09bcdc62e7dd0ad1c90c3
diego-c <diechaves@gmail.com>
2018-01-20 17:32:36 -0300

    handle contact data input

--- section-12/src/containers/Checkout/ContactData/ContactData.js
+++ section-12/src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import classes from './ContactData.css';
 
 class ContactData extends Component {
     state = {
+        price: 0,
         fields: {
             name: '',
             address: { 
@@ -17,15 +18,65 @@ class ContactData extends Component {
     orderHandler = e => {
         e.preventDefault();
         // Here we have access to ingredients and the total price easily via props!
-        console.log(this.props);
+        //console.log(this.props);
+        console.log('To be sent: ', this.state);
     }
 
     componentDidMount() {
+        this.setState({ price: this.props.price });
+        
         window.scroll({
             top: document.body.scrollHeight,
             left: 0,
             behavior: 'smooth'
         })
+    }
+
+    handleInput = e => {
+        let updatedValue = e.target.value;
+        console.log(`Updated value: ${updatedValue}`);
+
+        switch(e.target.id) {
+
+            case 'name':
+                this.setState(prevState => {
+                    const updatedState = { ...prevState };
+                    const updatedFields = { ...updatedState.fields }
+                    updatedFields.name = updatedValue
+                    return { fields: updatedFields }
+                })
+                break;
+
+            case 'email':
+                this.setState(prevState => {
+                    const updatedState = { ...prevState };
+                    const updatedFields = { ...updatedState.fields }
+                    updatedFields.email = updatedValue
+                    return { fields: updatedFields }
+                })
+                break;
+
+            case 'street':
+                this.setState(prevState => {
+                    const updatedState = { ...prevState };
+                    const updatedFields = { ...updatedState.fields }
+                    updatedFields.address.street = updatedValue
+                    return { fields: updatedFields }
+                })
+                break;
+
+            case 'postalCode':
+                this.setState(prevState => {
+                    const updatedState = { ...prevState };
+                    const updatedFields = { ...updatedState.fields }
+                    updatedFields.address.postalCode = updatedValue
+                    return { fields: updatedFields }
+                })
+                break;
+
+            default:
+                console.log('Oops, could not update field!');        
+        }
     }
 
     render() {
@@ -34,18 +85,40 @@ class ContactData extends Component {
                 <h4>Enter your contact data below</h4>
                 <form action="">
                     <label htmlFor="name">
-                        Name: <input id="name" type="text" placeholder="Your name..." />
+                        Name: 
+                        <input 
+                        id="name"
+                        type="text"
+                        placeholder="Your name..."
+                        value = { this.state.fields.name }
+                        onChange = { this.handleInput } />
                     </label>
                     <label htmlFor="email">
-                        E-mail: <input id="email" type="email" placeholder="Your e-mail..." />
+                        E-mail: 
+                        <input
+                        id="email"
+                        type="email"
+                        placeholder="Your e-mail..."
+                        value = { this.state.fields.email }
+                        onChange = { this.handleInput } />
                     </label>
                     <label htmlFor="address">
                         Address: 
                         <label htmlFor="street">
-                            Street: <input id = "street" type="text" placeholder="St..." />
+                            Street: 
+                            <input
+                            id = "street"
+                            type="text"
+                            placeholder="St..."
+                            onChange = { this.handleInput } />
                         </label>
                         <label htmlFor="postalCode">
-                            Postal Code: <input id = "postalCode" type="number" placeholder="ZIP..." />
+                            Postal Code: 
+                            <input
+                            id = "postalCode"
+                            type="number"
+                            placeholder="ZIP..."
+                            onChange = { this.handleInput } />
                         </label>
                     </label>
                     <Button
