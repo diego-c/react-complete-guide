fe908991536ad694ce0dfd893fc941918ef29df6
diego-c <diechaves@gmail.com>
2018-01-21 18:36:40 -0300

    fetch orders from firebase

--- section-12/src/components/Order/Order.js
+++ section-12/src/components/Order/Order.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = props => (
+const order = props => {
+    console.log(props);
+
+    return (
     <div className = { classes.Order }>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>$5.45</strong></p>
+        <p>To: <strong>{ props.customer }</strong></p>
+        <p>Ingredients:</p>
+        <ul>
+            { Object.keys(props.ingredients).map((ing, key) => (
+                props.ingredients[ing] ? (
+                <li 
+                key= { key }>
+                { ing[0].toUpperCase().concat(ing.slice(1)) } {`(${ props.ingredients[ing] })`}
+                </li>
+                ) : null 
+            )) }
+        </ul>
+        <p>Price: <strong>${ props.price.toFixed(2) }</strong></p>
+        <p>Delivery address: <strong>{ props.address.street }, { props.address.postalCode }</strong></p>
+        <p>Delivery method: <u><strong>{ props.delivery }</strong></u></p>
     </div>
-);
+    )
+};
 
 export default order;
--- section-12/src/containers/Orders/Orders.js
+++ section-12/src/containers/Orders/Orders.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import classes from './Orders.css';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-order';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 export default class Orders extends Component {
+    state = {
+        orders: null
+    }
+
+    // my own implementation of fetching orders from firebase
+    async componentDidMount() {
+        const orders = (await axios.get('/orders.json')).data;
+        this.setState({ orders });
+    }
+
     render() {
-        return (
-            <div className = { classes.Orders }>
-                <Order />
-                <Order />
-            </div>
-        )
+        if (this.state.orders) console.log(Object.entries(this.state.orders).map(arr => arr[1]));
+
+        let ordersOrSpinner =
+
+            this.state.orders ?
+            (
+                <div className = { classes.Orders }>
+                    { Object.entries(this.state.orders).map(arr => arr[1]).map((order, index) => (
+                        <Order
+                        key = { index }
+                        ingredients = { order.ingredients }
+                        price = { order.price }
+                        delivery = { order.fastDelivery ? 'fast' : 'normal' }
+                        address = { order.customer.address }
+                        customer = { order.customer.name } />
+                    )) }
+                </div>
+            ) : <Spinner />
+        
+
+        return ordersOrSpinner;
     }
 }
