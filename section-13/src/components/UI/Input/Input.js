66247c72f4e1d4fdabb9da1bcffd5fa4abb09870
diego-c <diechaves@gmail.com>
2018-01-21 21:22:07 -0300

    handle different input types - section 13 lecture 218

--- section-13/src/components/UI/Input/Input.js
+++ section-13/src/components/UI/Input/Input.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const input = props => {
-    
+    let inputElement = null;
+
+    switch (props.inputType) {
+
+        case 'input':
+        inputElement = 
+        <input { ...props }/>
+        break;
+        
+        case 'textarea':
+        inputElement = 
+        <textarea { ...props } />
+        break;        
+
+        default:
+        inputElement = <p>Oops, input not recognized!</p>;        
+    }
+
+    return (
+        <div>
+            <label htmlFor={ props.label.toLowercase() }>{ props.label }: { inputElement }</label>
+        </div>
+    )
 };
 
 export default input;
