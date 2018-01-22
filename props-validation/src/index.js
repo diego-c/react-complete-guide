691dba3c916b5ed7c579a93e23c188512c68bdf6
diego-c <diechaves@gmail.com>
2018-01-21 21:26:08 -0300

    branch out section 13

--- props-validation/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-
-render(<App title="Persons App" />, document.getElementById('root'));
-registerServiceWorker();
