a7986417862e52d725671aadf7a5ea4647cca381
diego-c <diechaves@gmail.com>
2018-01-25 13:54:07 -0300

    add reducer, store and action dispatchers

---
+++ section-14/redux-basics.js
@@ -0,0 +1,39 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer 
+const rootReducer = (state = initialState, action) => {
+    
+        if (action.type === 'INC_COUNTER') {
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        } 
+
+        if (action.type === 'ADD_COUNTER') {
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        }   
+
+    return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+// Note: the property 'type' is mandatory!
+store.dispatch({ type: 'INC_COUNTER' })
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+
+console.log(store.getState());
+
+// Subscription
