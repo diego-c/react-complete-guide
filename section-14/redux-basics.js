5164dea81ec1bf073effeed45002e4a10bb2a087
diego-c <diechaves@gmail.com>
2018-01-25 16:47:56 -0300

    add subscription - section 14 lecture 239

--- section-14/redux-basics.js
+++ section-14/redux-basics.js
@@ -29,11 +29,14 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+})
+
 // Dispatching Action
 // Note: the property 'type' is mandatory!
 store.dispatch({ type: 'INC_COUNTER' })
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 
 console.log(store.getState());
-
-// Subscription
