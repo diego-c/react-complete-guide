85a2590b69d460184ea26032986580dc3a52a7be
diego-c <diechaves@gmail.com>
2018-01-27 18:05:01 -0300

    clean up counter

--- section-14/src/containers/Counter/Counter.js
+++ section-14/src/containers/Counter/Counter.js
@@ -4,29 +4,7 @@ import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
-class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-            default:
-                return;
-        }
-    }
+class Counter extends Component {   
 
     render () {
         return (
