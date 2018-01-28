9bea4a1b2fe12c0e7a6d62ccd3de170f0fb51c7c
diego-c <diechaves@gmail.com>
2018-01-27 23:32:31 -0300

    add solution to redux-assignment-2

--- redux--assignment-2-problem/src/index.js
+++ redux--assignment-2-problem/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store = { store }><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
