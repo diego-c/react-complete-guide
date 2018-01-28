a1cc16f7621c5c4c578a0f80227c3d2cec50a2ba
diego-c <diechaves@gmail.com>
2018-01-27 23:11:10 -0300

    clean up code

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -5,26 +5,6 @@ import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-   /*  state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    } */
 
     render () {
         return (
