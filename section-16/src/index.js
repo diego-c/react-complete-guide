5305944fd94d1687fc61a6fbd569525b88709deb
diego-c <diechaves@gmail.com>
2018-02-02 21:00:32 -0300

    introducing middleware

--- section-16/src/index.js
+++ section-16/src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import reducer from './store/reducers/rootReducer';
 
-const store = createStore(reducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('Logger middleware dispatching:', action);
+            const result = next(action);
+            console.log('Current state:', store.getState())
+            return result;
+        }
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store = { store }><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
