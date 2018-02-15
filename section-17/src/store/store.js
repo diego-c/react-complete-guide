16a32bd691f5817b9e56cd03dc680b432ab20a4e
diego-c <diechaves@gmail.com>
2018-02-15 19:32:16 -0300

    add redux devtools - section 17 lecture 281

--- section-17/src/store/store.js
+++ section-17/src/store/store.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
 import reducer from './reducers/rootReducer';
 
-export default createStore(reducer);
+export default createStore(reducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
