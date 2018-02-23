6b77337b06fd974527c34633ac1ab8093111a891
diego-c <diechaves@gmail.com>
2018-02-23 20:43:20 -0300

    use redux devtools only in development - section 19 lecture 329

--- section-19/src/store/store.js
+++ section-19/src/store/store.js
@@ -2,6 +2,6 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 export default createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
