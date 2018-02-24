297e06a3b16f3c050815bc4ef9f53252a03cac60
diego-c <diechaves@gmail.com>
2018-02-24 13:00:41 -0300

    add beforeEach

--- section-20/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ section-20/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,18 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    })
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({ isAuth: true });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
     })
 })
