d121eca4266fe196c70550943f4eca4e0ae9ca9b
diego-c <diechaves@gmail.com>
2018-02-24 12:26:31 -0300

    write first test - section 20 lecture 338

---
+++ section-20/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({ adapter: new Adapter() });
+
+describe('<NavigationItems />', () => {
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+})
